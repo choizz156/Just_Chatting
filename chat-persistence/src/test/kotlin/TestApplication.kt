b143cfd14689536db869d0bb7cc638fd55633762
choizz156 <tkfka156@gmail.com>
2025-09-19 19:47:12 +0900

    test: 회원가입 테스트 코드 작성

---
+++ chat-persistence/src/test/kotlin/TestApplication.kt
@@ -0,0 +1,20 @@
+package com.chat.persistence.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@SpringBootApplication(scanBasePackages = ["com.chat"])
+@EntityScan(basePackages = ["com.chat.core.domain.entity"])
+@EnableJpaRepositories(basePackages = ["com.chat.persistence.repository"])
+class TestApplication {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+}
---
+++ chat-persistence/src/test/kotlin/application/UserServiceV1Test.kt
@@ -0,0 +1,38 @@
+package application
+
+import com.chat.core.application.UserService
+import com.chat.core.application.dto.CreateUserContext
+import com.chat.core.domain.entity.UserRole
+import com.chat.persistence.repository.UserRepository
+import com.chat.persistence.test.TestApplication
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.test.Test
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = [TestApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserServiceV1Test(
+    @Autowired val userServiceV1: UserService,
+    @Autowired val userRepository: UserRepository
+) {
+
+    @BeforeEach
+    fun setUp() {
+        userRepository.deleteAllInBatch()
+    }
+
+    @Test
+    fun testCreate() {
+        val createUserContext = CreateUserContext("test@gmail.com", "dsfsdf123", "test11")
+
+        val createUser = userServiceV1.createUser(createUserContext)
+
+        assertThat(userRepository.count()).isEqualTo(1)
+        assertThat(createUser.email).isEqualTo(createUserContext.email)
+        assertThat(createUser.nickname).isEqualTo(createUserContext.nickname)
+        assertThat(createUser.roles).isEqualTo(UserRole.USER)
+    }
+}
